079eaf93646f346f2ac5e67c0b0806a15ccba2df
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-01-19 23:36:48 +0530

    Update MainSceneController

--- src/main/java/lk/ijse/dep13/hotelfeeapp/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep13/hotelfeeapp/controller/MainSceneController.java
@@ -2,18 +2,29 @@ package lk.ijse.dep13.hotelfeeapp.controller;
 
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.text.Text;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class MainSceneController {
     public DatePicker checkIn;
     public DatePicker checkOut;
-    public Spinner<?> spnCount;
+    public Spinner<Integer> spnCount;
     public Text txtTotal;
 
+    private static final BigDecimal PERDAYCOST = new BigDecimal("10000");
+
     public void initialize() {
-        checkIn = new DatePicker();
-        checkOut = new DatePicker();
-        spnCount = new Spinner();
+        spnCount.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 10, 0));
+
+        LocalDate checkInDate = checkIn.getValue();
+        checkIn.valueProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println(newValue);
+
+        });
+
         txtTotal.setText("0.00");
     }
 }
