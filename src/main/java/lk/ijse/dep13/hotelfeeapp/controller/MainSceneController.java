710e1497d8c666d791019ab3f2e2937d0a15a508
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-01-19 21:27:29 +0530

    Update MainSceneController

--- src/main/java/lk/ijse/dep13/hotelfeeapp/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep13/hotelfeeapp/controller/MainSceneController.java
@@ -1,4 +1,19 @@
 package lk.ijse.dep13.hotelfeeapp.controller;
 
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.text.Text;
+
 public class MainSceneController {
+    public DatePicker checkIn;
+    public DatePicker checkOut;
+    public Spinner<?> spnCount;
+    public Text txtTotal;
+
+    public void initialize() {
+        checkIn = new DatePicker();
+        checkOut = new DatePicker();
+        spnCount = new Spinner();
+        txtTotal.setText("0.00");
+    }
 }
